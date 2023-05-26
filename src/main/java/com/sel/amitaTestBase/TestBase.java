9fae00f929b4d691051c0ff7996851958698c919
sum1982 <66497471+sum1982@users.noreply.github.com>
2023-05-26 11:30:18 -0500

    Update TestBase.java

--- src/main/java/com/sel/amitaTestBase/TestBase.java
+++ src/main/java/com/sel/amitaTestBase/TestBase.java
@@ -7,6 +7,7 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
